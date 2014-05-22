2642316127100d0bc370796f136bb98b18793d05
Olivier Hoareau <olivier@phppro.fr>
2014-05-22 23:05:04 +0200

    fix

--- manifests/agent/backup.pp
+++ manifests/agent/backup.pp
@@ -4,7 +4,7 @@ class p::agent::backup (
 
   anchor {'p::agent::backup::begin': }
 
-  anchor {'p::agent::backup::begin':
+  anchor {'p::agent::backup::end':
     require => Anchor['p::agent::backup::begin'],
   }
 
