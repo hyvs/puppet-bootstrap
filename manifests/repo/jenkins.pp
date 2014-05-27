d6d58976f95acc8c9b153cc5d9b1e967a65e0b5c
Olivier Hoareau <olivier@phppro.fr>
2014-05-28 00:00:19 +0200

    fix

--- manifests/repo/jenkins.pp
+++ manifests/repo/jenkins.pp
@@ -4,7 +4,7 @@ class p::repo::jenkins (
   anchor {'p::repo::jenkins::begin': } ->
   p::resource::apt::repo {'jenkins':
     location    => 'http://pkg.jenkins-ci.org/debian',
-    release     => 'binary',
+    release     => 'binary/',
     repos       => '',
     key         => 'D50582E6',
     key_source  => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key',
