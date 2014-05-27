608542094374f458b7d43cb84d526ac64a3c2c5c
Olivier Hoareau <olivier@phppro.fr>
2014-05-28 00:36:52 +0200

    fix

--- manifests/agent/backup.pp
+++ manifests/agent/backup.pp
@@ -4,6 +4,68 @@ class p::agent::backup (
 
   anchor {'p::agent::backup::begin': }
 
+  p::resource::package {'nfs-common':
+    require => Anchor['p::agent::backup::begin'],
+    before  => Anchor['p::agent::backup::end'],
+  }
+
+  file {'/root/bin':
+    ensure => directory,
+  }
+
+  file {'/backups':
+    ensure => directory,
+    require => Anchor['p::agent::backup::begin'],
+  }
+
+  file {'/root/bin/backup-daily.sh':
+    ensure  => file,
+    mode    => 700,
+    content => template('p/backup/daily.sh.erb'),
+    require => [File['/root/bin'], File['/backups'], Anchor['p::agent::backup::begin']],
+    before  => Anchor['p::agent::backup::end'],
+  }
+
+  file {'/root/bin/backup-evening.sh':
+    ensure  => file,
+    mode    => 700,
+    content => template('p/backup/evening.sh.erb'),
+    require => [File['/root/bin'], File['/backups'], Anchor['p::agent::backup::begin']],
+    before  => Anchor['p::agent::backup::end'],
+  }
+
+  file {'/root/bin/backup-hourly.sh':
+    ensure  => file,
+    mode    => 700,
+    content => template('p/backup/hourly.sh.erb'),
+    require => [File['/root/bin'], File['/backups'], Anchor['p::agent::backup::begin']],
+    before  => Anchor['p::agent::backup::end'],
+  }
+
+  file {'/root/bin/backup-mid-day.sh':
+    ensure  => file,
+    mode    => 700,
+    content => template('p/backup/mid-day.sh.erb'),
+    require => [File['/root/bin'], File['/backups'], Anchor['p::agent::backup::begin']],
+    before  => Anchor['p::agent::backup::end'],
+  }
+
+  file {'/root/bin/backup-morning.sh':
+    ensure  => file,
+    mode    => 700,
+    content => template('p/backup/morning.sh.erb'),
+    require => [File['/root/bin'], File['/backups'], Anchor['p::agent::backup::begin']],
+    before  => Anchor['p::agent::backup::end'],
+  }
+
+  file {'/root/bin/backup-night.sh':
+    ensure  => file,
+    mode    => 700,
+    content => template('p/backup/night.sh.erb'),
+    require => [File['/root/bin'], File['/backups'], Anchor['p::agent::backup::begin']],
+    before  => Anchor['p::agent::backup::end'],
+  }
+
   anchor {'p::agent::backup::end':
     require => Anchor['p::agent::backup::begin'],
   }
--- manifests/server/backup.pp
+++ manifests/server/backup.pp
@@ -1,11 +1,7 @@
 class p::server::backup (
 ) {
 
-  anchor {'p::server::backup::begin': }
-
-  # todo
-
-  anchor {'p::server::backup::end':
-    require => Anchor['p::server::backup::begin'],
-  }
+  anchor {'p::server::backup::begin': } ->
+  p::resource::package {'nfs-kernel-server': } ->
+  anchor {'p::server::backup::end': }
 }
