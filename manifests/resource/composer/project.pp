b8a75eeef3102b7b603d6d63adf555c6265f616b
Olivier Hoareau <olivier@phppro.fr>
2014-06-02 19:09:09 +0200

    fix

--- manifests/resource/composer/project.pp
+++ manifests/resource/composer/project.pp
@@ -1,11 +1,13 @@
 define p::resource::composer::project (
-  $dir = $name,
-  $user = 'root'
+  $dir  = $name,
+  $user = 'root',
+  $composer_home = '/root/.composer'
 ) {
 
   exec {"composer install ${dir}" :
     cwd     => $dir,
     user    => $user,
+    environment => ["COMPOSER_HOME=${composer_home}"],
     command => "composer install --no-interaction",
   }
 
