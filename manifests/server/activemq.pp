b08434d5aea542d65f1f30d87e914375dd536fcf
Olivier Hoareau <olivier@phppro.fr>
2014-06-11 12:18:43 +0200

    fix activemq

--- manifests/server/activemq.pp
+++ manifests/server/activemq.pp
@@ -9,6 +9,11 @@ class p::server::activemq (
     install_dependencies => false,
     install              => 'package',
     template             => 'mcollective/activemq.xml.erb',
+    config_file          => '/etc/activemq/instances-enabled/main/activemq.xml',
+  } ->
+  file {'/usr/share/activemq/jetty.xml':
+    ensure  => 'file',
+    content => template('p/activemq/jetty.xml.erb'),
   } ->
   p::resource::firewall::tcp {'activemq':
     port    => $port,
