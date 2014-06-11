05c5f11049fb49cc5ec4b158f7f3da00a685383a
Olivier Hoareau <olivier@phppro.fr>
2014-06-11 16:47:53 +0200

    add capistrano endpoint

---
+++ manifests/endpoint/capistrano.pp
@@ -0,0 +1,24 @@
+class p::endpoint::capistrano (
+  $applications         = hiera('capistrano_applications'),
+  $application_resource = 'p::resource::capistrano::application',
+  $dirs                 = hiera('dirs'),
+  $home_owner           = undef,
+  $home_group           = undef
+) {
+
+  $home = $dirs['capistrano.home']
+
+  $applications_defaults = {
+    require => [P::Resource::Directory[$home], File[$home]],
+    before  => Anchor['p::endpoint::capistrano::end']
+  }
+
+  anchor {'p::endpoint::capistrano::begin': } ->
+  p::resource::directory {$home:
+    owner => $home_owner,
+    group => $home_group
+  } ->
+  anchor {'p::endpoint::capistrano::end': }
+
+  create_resources($application_resources, $applications, $applications_defaults)
+}
---
+++ manifests/resource/capistrano/application.pp
@@ -0,0 +1,43 @@
+define p::resource::capistrano::application (
+  $dir,
+  $require_dir = undef,
+  $before_apache_vhost = undef,
+  $owner = undef,
+  $group = undef
+) {
+
+  if undef != $require_dir {
+    anchor {"p::resource::capistrano::application::${title}::require_dir":
+      require => File[$require_dir],
+    }
+  } else {
+    anchor {"p::resource::capistrano::application::${title}::require_dir":
+    }
+  }
+
+  if undef != $before_apache_vhost {
+    anchor {"p::resource::capistrano::application::${title}::before_apache_vhost":
+      require => P::Resource::Apache::Vhost[$before_apache_vhost],
+    }
+  } else {
+    anchor {"p::resource::capistrano::application::${title}::before_apache_vhost":
+    }
+  }
+
+  p::resource::directory {$dir:
+    owner   => $owner,
+    group   => $group,
+    require => Anchor["p::resource::capistrano::application::${title}::require_dir"],
+  } ->
+  p::resource::directory {"${dir}/empty":
+    owner => $owner,
+    group => $group
+  } ->
+  p::resource::link {"${dir}/current":
+    owner  => $owner,
+    group  => $group,
+    target => "${dir}/empty",
+    before => Anchor["p::resource::capistrano::application::${title}::before_apache_vhost"],
+  }
+
+}
