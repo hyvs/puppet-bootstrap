4b95bc2e9beaf969878156c185dd0f79789bd862
Olivier Hoareau <olivier@phppro.fr>
2014-06-11 10:49:51 +0200

    fix activemq

--- manifests/server/activemq.pp
+++ manifests/server/activemq.pp
@@ -1,12 +1,13 @@
 class p::server::activemq (
   $firewall = true,
   $port     = 61613,
-  $version  = '5.5.1'
+  $version  = '5.10.0'
 ) {
 
   anchor {'p::server::activemq::begin': } ->
   class {'::activemq':
     install_dependencies => false,
+    install_source       => "http://www.eu.apache.org/dist/activemq/${version}/apache-activemq-${version}-bin.zip",
     install              => 'source',
     version              => $version,
     template             => 'mcollective/activemq.xml.erb',
