d0e717e0f7f09f22d83bc771e023fe064046d52d
Olivier Hoareau <olivier@phppro.fr>
2014-06-13 22:32:28 +0200

    add autofs and nfs support

--- manifests/resource/nfs/export.pp
+++ manifests/resource/nfs/export.pp
@@ -2,7 +2,7 @@ define p::resource::nfs::export (
   $dir,
   $acls
 ) {
-  file {"/etc/exports.d/${name}":
+  file {"/etc/exports.d/${name}.exports":
     content => template('p/nfs/export.erb'),
     require => File['/etc/exports.d'],
   }
