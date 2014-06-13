0548bee459ee96157ea7316787502787d58550ee
Olivier Hoareau <olivier@phppro.fr>
2014-06-13 22:29:24 +0200

    add autofs and nfs support

--- manifests/agent/autofs.pp
+++ manifests/agent/autofs.pp
@@ -4,12 +4,14 @@ class p::agent::autofs (
 ) {
 
   $roots_defaults = {
-    require => Anchor['p::agent::autofs::begin'],
+    require => P::Resource::Package['/etc/auto.master.d'],
     before  => Anchor['p::agent::autofs::end']
   }
 
   anchor {'p::agent::autofs::begin': } ->
   p::resource::package {'autofs': } ->
+  p::resource::package {'/etc/auto.master.d':
+  } ->
   anchor {'p::agent::autofs::end': }
 
   create_resources($root_resource, $roots, $roots_defaults)
