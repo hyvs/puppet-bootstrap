2a018e5d3b0ffc0f8a07995fc012089086cbf955
Olivier Hoareau <olivier@phppro.fr>
2014-06-15 18:19:22 +0200

    fix backup

--- manifests/resource/backup/cron.pp
+++ manifests/resource/backup/cron.pp
@@ -14,7 +14,7 @@ define p::resource::backup::cron (
 
   $shell_file   = "${script_dir}/${script_prefix}${name}"
 
-  $forced_options = {frequency => "${minute} ${hour} ${date} ${month} ${weekday}"}
+  $forced_options = {cron_name => $name, frequency => "${minute} ${hour} ${date} ${month} ${weekday}"}
 
   if is_hash($options) {
     $real_options = merge($options, $forced_options)
