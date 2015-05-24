3d7aa4c2c32f7195b28d027d727f2bb6bd5f7f0d
Olivier Hoareau <olivier@phppro.fr>
2015-05-24 16:14:19 +0200

    add network/interfaces.d support (debian 8)

---
+++ manifests/resource/network/interface.pp
@@ -0,0 +1,15 @@
+define p::resource::network::interface (
+  ip,
+  netmask,
+  broadcast,
+  $comment   = undef,
+  $auto      = true,
+  type       = 'static',
+  $dir       = '/etc/network/interfaces.d'
+) {
+
+  file {"${dir}/${name}.cfg":
+    content => template('p/network/interface.cfg.erb'),
+  }
+
+}
--- manifests/system/network.pp
+++ manifests/system/network.pp
@@ -1,28 +1,50 @@
 class p::system::network (
-  $interfaces = hiera_hash('network_interfaces')
+  $interfaces       = hiera_hash('network_interfaces'),
+  $extra_interfaces = hiera_hash('extra_network_interfaces'),
+  $resource         = 'p::resource::network::interface'
 ) {
 
-  $interfaces_file = "/etc/network/interfaces"
-
-  if !$interfaces['lo'] {
-    fail("No lo interfaces defined")
-  }
-
-  if !$interfaces['eth0'] {
-    fail("No eth0 interfaces defined")
-  }
+  $interfaces_file      = "/etc/network/interfaces"
+  $extra_interfaces_dir = "/etc/network/interfaces.d"
 
   anchor {'p::system::network::begin': } ->
-  p::resource::file {$interfaces_file:
-    template => "p/network/interfaces.erb",
-    vars     => {
-      interfaces => $interfaces
-    },
-  } ->
-  exec {'reload network':
-    command => "sudo service networking stop ; sudo service networking start",
-  } ->
-  anchor {'p::system::network::end':
+  anchor {'p::system::network::reload': } ->
+  exec {'reload network': command => "sudo service networking restart"} ->
+  anchor {'p::system::network::end': }
+
+  if !empty($interfaces) {
+    if !$interfaces['lo'] {
+      fail("No lo interfaces defined")
+    }
+
+    if !$interfaces['eth0'] {
+      fail("No eth0 interfaces defined")
+    }
+
+    p::resource::file {$interfaces_file:
+      template => "p/network/interfaces.erb",
+      vars     => {
+        interfaces => $interfaces
+      },
+      require => Anchor['p::system::network::begin'],
+      before  => Anchor['p::system::network::reload'],
+    }
+  }
+
+  if !empty($extra_interfaces) {
+    file_line { 'interfaces.d support enable':
+      path  => $interfaces_file,
+      line  => "source ${extra_interfaces_dir}/*.cfg",
+      require => Anchor['p::system::network::begin'],
+      before  => Anchor['p::system::network::reload'],
+    }
+
+    $defaults = {
+      require => File_line['interfaces.d support enable'],
+      before  => Anchor['p::system::network::reload'],
+    }
+
+    create_resources($resource, $extra_interfaces, $defaults)
   }
 
 }
