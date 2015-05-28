695e6bc65db107c332e7780293ac00c727389b0f
Olivier Hoareau <olivier@phppro.fr>
2015-05-28 21:15:58 +0200

    refactoring firewall

--- manifests/repo/newrelic.pp
+++ manifests/repo/newrelic.pp
@@ -6,7 +6,7 @@ class p::repo::newrelic (
     release    => 'newrelic',
     repos      => 'non-free',
     key        => 'B31B29E5548C16BF',
-    key_server => 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xB31B29E5548C16BF',
+    key_server => 'http://keyserver.ubuntu.com/pks/lookup?op=get\\&search=0xB31B29E5548C16BF',
   }
 
 }
