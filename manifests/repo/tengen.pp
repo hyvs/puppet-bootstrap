e0d3f52699b71074a862db062104bd4eba1b747a
Olivier Hoareau <olivier@phppro.fr>
2015-06-05 10:16:11 +0200

    10gen repo only for wheezy

--- manifests/repo/tengen.pp
+++ manifests/repo/tengen.pp
@@ -3,7 +3,7 @@ class p::repo::tengen (
 
   p::resource::apt::repo { '10gen':
     location    => 'http://repo.mongodb.org/apt/debian',
-    release     => 'jessie/mongodb-org/3.0',
+    release     => 'wheezy/mongodb-org/3.0',
     repos       => 'main',
     key         => '9ECBEC467F0CEB10',
     key_server  => 'http://docs.mongodb.org/10gen-gpg-key.asc',
