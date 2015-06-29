01f717dcd37a6017fd3b0c5dd12ca40c87fe0b42
Olivier Hoareau <olivier@phppro.fr>
2015-06-29 12:02:37 +0200

    +

--- manifests/resource/phpfpm/pool.pp
+++ manifests/resource/phpfpm/pool.pp
@@ -1,14 +1,21 @@
 define p::resource::phpfpm::pool (
-  $listen = undef
+  $listen,
+  $listen_backlog = -1,
+  $listen_allowed_clients = "127.0.0.1",
+  $listen_mode = "0600",
+  $user = "www-data",
+  $group = "www-data",
+  $pm = "dynamic",
+  $pm_max_children = 5,
+  $pm_min_spare_servers = 1,
+  $pm_max_spare_servers = 3,
+  $pm_max_requests = 500,
+  $pm_status_path = undef,
+  $ping_path = undef,
+  $request_slowlog_timeout = "10s"
 ) {
 
-  if !$listen {
-    $real_listen = "/var/run/php5-fpm-${name}.sock"
-  } else {
-    $real_listen = $listen
-  }
-
-  $conf_file = "/etc/php5/fpm/pool.d/${name}.conf"
+  $conf_file   = "/etc/php5/fpm/pool.d/${name}.conf"
 
   file { $conf_file:
     ensure  => file,
