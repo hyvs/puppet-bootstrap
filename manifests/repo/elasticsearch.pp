0da6641cbf293564caa9814ff65bf6432afb0bc3
Olivier Hoareau <olivier@phppro.fr>
2015-07-01 23:18:35 +0200

    +

---
+++ manifests/repo/elasticsearch.pp
@@ -0,0 +1,12 @@
+class p::repo::elasticsearch (
+) {
+
+  p::resource::apt::repo { 'elasticsearch':
+    location   => 'http://packages.elasticsearch.org/logstash/1.6/debian',
+    release    => 'stable',
+    repos      => 'main',
+    key        => 'D27D666CD88E42B4',
+    key_server => 'https://packages.elasticsearch.org/GPG-KEY-elasticsearch',
+  }
+
+}
