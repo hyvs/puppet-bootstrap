ba92045845182025dd13782f39d0402134715723
Olivier Hoareau <olivier@phppro.fr>
2015-07-01 23:25:17 +0200

    +

--- manifests/repo/elasticsearch.pp
+++ manifests/repo/elasticsearch.pp
@@ -2,7 +2,7 @@ class p::repo::elasticsearch (
 ) {
 
   p::resource::apt::repo { 'elasticsearch':
-    location   => 'http://packages.elasticsearch.org/logstash/1.6/debian',
+    location   => 'http://packages.elasticsearch.org/elasticsearch/1.6/debian',
     release    => 'stable',
     repos      => 'main',
     key        => 'D27D666CD88E42B4',
