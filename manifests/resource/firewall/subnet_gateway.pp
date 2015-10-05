8fbecbdff0b92417e2b97a1ba047f49ae7c2abeb
Olivier Hoareau <olivier@phppro.fr>
2015-10-05 02:34:13 +0200

    +

--- manifests/resource/firewall/subnet_gateway.pp
+++ manifests/resource/firewall/subnet_gateway.pp
@@ -6,7 +6,6 @@ define p::resource::firewall::subnet_gateway (
     firewall { "200 use ${interface} as outgoing gateway from ${subnet}":
       table   => 'nat',
       chain   => 'POSTROUTING',
-      action  => 'accept',
       proto   => 'all',
       jump    => 'MASQUERADE',
       source  => $subnet,
