1bfad978038acf4b631361f3f1723b6a980df4e3
Olivier Hoareau <olivier@phppro.fr>
2015-10-18 21:29:04 +0200

    add pre_requisites

--- manifests/server/mongodb.pp
+++ manifests/server/mongodb.pp
@@ -47,11 +47,16 @@ class p::server::mongodb (
   }
 
   if $data_dir {
+      if defined(P::Resource::Directory[$data_dir]) {
+        $pre_requisites = [P::Resource::Directory[$data_dir], P::Resource::Package['mongodb-org'], P::Resource::Package['mongodb-org-server']]
+      } else {
+        $pre_requisites = [P::Resource::Package['mongodb-org'], P::Resource::Package['mongodb-org-server']]
+      }
        exec { "move mongodb data dir to custom location ${data_dir} if need":
          cwd     => "${data_dir}",
          command => "service mongod stop; cp -aR /var/lib/mongodb/* ${data_dir}/",
          unless  => "[ -f ${data_dir}/storage.bson ]",
-         require => [P::Resource::Package['mongodb-org'], P::Resource::Package['mongodb-org-server']],
+         require => $pre_requisites,
          notify  => Service['mongod'],
        }
     -> file_line { "mongodb set custom db path to ${data_dir}":
