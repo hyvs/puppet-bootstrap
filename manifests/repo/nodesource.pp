bd3bb65f9944098a37e76c31bf4b5d3552c5e3c9
Olivier Hoareau <olivier@phppro.fr>
2015-10-19 10:44:47 +0200

    +

--- manifests/repo/nodesource.pp
+++ manifests/repo/nodesource.pp
@@ -2,8 +2,8 @@ class p::repo::nodesource (
 ) {
 
   p::resource::apt::repo { 'nodesource':
-    location   => 'https://deb.nodesource.com',
-    release    => 'node_0.12',
+    location   => 'https://deb.nodesource.com/node_0.12',
+    release    => 'jessie',
     repos      => 'main',
     key        => '1655A0AB68576280',
     key_server => 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key',
